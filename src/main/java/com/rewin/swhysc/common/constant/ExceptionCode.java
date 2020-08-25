84c117fe0991f4a768656e3bdedae75bb507ec1b
sinan <sinan@rewin.com.cn>
2020-08-25 10:02:58 +0800

    需求-前台软件信息查询

---
+++ src/main/java/com/rewin/swhysc/common/constant/ExceptionCode.java
@@ -0,0 +1,10 @@
+package com.rewin.swhysc.common.constant;
+
+/**
+ * @program: swhyscManageServer
+ * @description:异常代码定义文件
+ * @author: sinan@rewin.com.cn
+ * @create: 2020/8/24 16:39
+ **/
+public class ExceptionCode {
+}
